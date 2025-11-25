cda4519e034ee1136d619045a4ccdc829a4f385f
MK-PANKAJ <manishkumarpankaj119@gmail.com>
2025-11-25 22:27:00 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -299,7 +299,7 @@ const Home = () => {
                   I'm a Student – Start Now <ArrowRight className="ml-2 h-5 w-5" />
                 </Link>
               </Button>
-              <Button asChild size="lg" variant="outline" className="border-2 border-white text-white hover:bg-white/10 text-lg px-8 py-6">
+               <Button asChild size="lg" className="bg-white text-secondary hover:bg-white/90 text-lg px-8 py-6">
                 <Link to="/schools">
                   I'm a School – Get Info <ArrowRight className="ml-2 h-5 w-5" />
                 </Link>
