dadfdba9a80dbda559a7c05034f8dcf6411a32cf
MK-PANKAJ <manishkumarpankaj119@gmail.com>
2025-12-02 14:38:31 +0000

    main.tsx update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
+import React from 'react';
 import { createRoot } from "react-dom/client";
+// Change BrowserRouter to HashRouter
+import { HashRouter } from 'react-router-dom'; 
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>
+  <React.StrictMode>
+    <HelmetProvider>
+      {/* Use HashRouter instead of BrowserRouter. 
+         We don't need the `basename` prop with HashRouter, it handles it automatically.
+      */}
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </HelmetProvider>
+  </React.StrictMode>
 );
