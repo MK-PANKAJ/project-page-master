d4b836974e26acf370daf24e4febf2091c986bf2
MK-PANKAJ <manishkumarpankaj119@gmail.com>
2025-12-03 10:54:32 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,53 @@
-import React from 'react';
-import { createRoot } from "react-dom/client";
-// Change BrowserRouter to HashRouter
-import { HashRouter } from 'react-router-dom'; 
-import { HelmetProvider } from "react-helmet-async";
-import App from "./App.tsx";
-import "./index.css";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Routes, Route } from "react-router-dom"; // Use Routes and Route, remove BrowserRouter
+import { ErrorBoundary } from "@/components/ErrorBoundary";
+import { AnnouncementBanner } from "@/components/AnnouncementBanner";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Students from "./pages/Students";
+import Schools from "./pages/Schools";
+import Resources from "./pages/Resources";
+import Contact from "./pages/Contact";
+import Gallery from "./pages/Gallery";
+import Testimonials from "./pages/Testimonials";
+import Auth from "./pages/Auth";
+import Admin from "./pages/Admin";
+import Blog from "./pages/Blog";
+import BlogPost from "./pages/BlogPost";
+import NotFound from "./pages/NotFound";
 
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <HelmetProvider>
-      {/* Use HashRouter instead of BrowserRouter. 
-         We don't need the `basename` prop with HashRouter, it handles it automatically.
-      */}
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </HelmetProvider>
-  </React.StrictMode>
+const queryClient = new QueryClient();
+
+const App = () => (
+  <ErrorBoundary>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        {/* Removed BrowserRouter here because HashRouter is now in main.tsx */}
+        <AnnouncementBanner />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/students" element={<Students />} />
+          <Route path="/schools" element={<Schools />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/testimonials" element={<Testimonials />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/admin" element={<Admin />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </ErrorBoundary>
 );
+
+export default App;
